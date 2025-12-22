d994f55c756fa76971d282eac83ce60131714fc7
bala-amypo <bala@amypo.in>
2025-12-22 05:48:43 +0000

    User commit

--- src/main/java/com/example/demo/service/RepeatOffenderRecordService.java
+++ src/main/java/com/example/demo/service/RepeatOffenderRecordService.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
 @Service
 public class RepeatOffenderRecordService {
 
-public int computeRepeatOffenderRecord(int cases) {
-if (cases <= 1) return 1;
-if (cases == 2) return 2;
-return 4;
-}
+    public int computeRepeatOffenderRecord(int cases) {
+        if (cases <= 1) return 1;
+        if (cases == 2) return 2;
+        return 4;
+    }
 }
